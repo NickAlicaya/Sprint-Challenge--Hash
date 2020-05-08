39f8736ede37520a53ffec01742d39b77efca8db
nicanor-alicaya <donalicaya@gmail.com>
2020-05-08 14:57:11 -0400

    removed hashtable files that were preventing add . somehow

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,21 +1,22 @@
 def finder(files, queries):
-
-    """
-    YOUR CODE HERE
-    """
-
-    return result
-
-
-if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+  
+    cache = {}
+    results = []
+    
+    for i in files:
+       
+        key = i.split('/')
+        cache.setdefault(key[-1], []).append(i)    
+  
+    for j in queries:
+        if 'nofile' in j:
+            continue
+        if j in cache:
+            if len(cache[j]) > 1:
+                for i in cache[j]:
+                    results.append(i)
+            else:
+                results.append(cache[j][0])
+        else:
+            continue
+    return results
